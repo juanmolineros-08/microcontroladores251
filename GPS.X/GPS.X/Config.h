73c00c3fd2dc05f76e14ccf07d94d29d7f107cd9
Miguel Tapia <143210047+Miguel072@users.noreply.github.com>
2025-05-09 12:11:16 -0500

    Add files via upload

--- GPS.X/GPS.X/Config.h
+++ GPS.X/GPS.X/Config.h
@@ -1,53 +1,103 @@
-/* 
- * File:   Config.h
- * Author: usuario
- *
- * Created on 23 de abril de 2025, 02:12 PM
- */
+/*!
+\file   Config.h
+\date   2021-09-13
+\author Fulvio Vivas <fulvio.vivas@unicauca.edu.co>
+\brief  configuration bits pic18f4550.
 
-#ifndef CONFIG_H
-#define CONFIG_H
+\par Copyright
+Information contained herein is proprietary to and constitutes valuable
+confidential trade secrets of unicauca, and
+is subject to restrictions on use and disclosure.
+
+\par
+Copyright (c) unicauca 2021. All rights reserved.
+
+\par
+The copyright notices above do not evidence any actual or
+intended publication of this material.
+
+\note
+Last modified: 2025-03-26 Cristian Valencia <chpayan@unicauca.edu.co>
+******************************************************************************
+*/
 
 #include <xc.h>
 
-// Configuración del PIC18F4550
-#pragma config PLLDIV = 1       // No prescaler para el PLL (4MHz input)
-#pragma config CPUDIV = OSC1_PLL2// CPU clock divide por 2 (8MHz/2 = 4MHz)
-#pragma config USBDIV = 1       // USB clock desde oscilador primario
+#ifndef CONFIG_H
+#define	CONFIG_H
 
-#pragma config FOSC = INTOSC_EC // Oscilador interno, CLKO en RA6
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor desactivado
-#pragma config IESO = OFF       // Oscillator Switchover desactivado
-
-#pragma config PWRT = OFF       // Power-up Timer desactivado
-#pragma config BOR = ON         // Brown-out Reset activado
-#pragma config BORV = 3         // Voltage level 2.05V
-#pragma config VREGEN = OFF     // USB Voltage Regulator desactivado
-
-#pragma config WDT = OFF        // Watchdog Timer desactivado
-#pragma config WDTPS = 32768    // Postscaler value (no usado con WDT off)
-
-#pragma config CCP2MX = ON      // CCP2 multiplexado con RC1
-#pragma config PBADEN = OFF     // PORTB<4:0> como pines digitales
-#pragma config LPT1OSC = OFF    // Timer1 en modo high power
-#pragma config MCLRE = OFF      // MCLR desactivado, RE3 como entrada
-
-#pragma config STVREN = ON      // Stack Overflow Reset activado
-#pragma config LVP = OFF        // Low Voltage Programming desactivado
-#pragma config ICPRT = OFF      // Debug port desactivado
-#pragma config XINST = OFF      // Extended Instruction Set desactivado
-
-// Configuraciones de protección
-#pragma config CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF
-#pragma config CPB = OFF, CPD = OFF
-#pragma config WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF
-#pragma config WRTC = OFF, WRTB = OFF, WRTD = OFF
-#pragma config EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF
-#pragma config EBTRB = OFF
-
-// Configuración para UART y GPS
-#define _XTAL_FREQ 8000000      // Frecuencia del oscilador interno
-#define BAUDRATE 9600           // Mismo baudrate que el NEO-6M por defecto
-
+#ifdef	__cplusplus
+extern "C" {
 #endif
+
+    // CONFIG1L
+#pragma config PLLDIV = 1       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
+#pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
+#pragma config USBDIV = 1       // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
+
+// CONFIG1H
+#pragma config FOSC = INTOSC_EC // Oscillator Selection bits (Internal oscillator, CLKO function on RA6, EC used by USB (INTCKO))
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOR = ON         // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 3         // Brown-out Reset Voltage bits (Minimum setting 2.05V)
+#pragma config VREGEN = OFF     // USB Voltage Regulator Enable bit (USB voltage regulator disabled)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = ON      // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF      // PORTB A/D Enable bit (PORTB<4:0> pins are configured as analog input channels on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = OFF      // MCLR Pin Enable bit (RE3 input pin enabled; MCLR pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+#pragma config ICPRT = OFF      // Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
+
+#define _XTAL_FREQ 8000000
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONFIG_H */
+
 
--- GPS.X/GPS.X/LCD.h
+++ GPS.X/GPS.X/LCD.h
@@ -1,39 +1,59 @@
-/* 
- * File:   LCD.h
- * Author: usuario
- *
- * Created on 23 de abril de 2025, 02:12 PM
- */
+/*!
+\file   LCD.h
+\date   2021-09-13
+\author Fulvio Vivas <fulvio.vivas@unicauca.edu.co>
+\brief  configuration LCD 16X2 4BITS.
+
+\par Copyright
+Information contained herein is proprietary to and constitutes valuable
+confidential trade secrets of unicauca, and
+is subject to restrictions on use and disclosure.
+
+\par
+Copyright (c) unicauca 2021. All rights reserved.
+
+\par
+The copyright notices above do not evidence any actual or
+intended publication of this material.
+
+\note
+Last modified: 2025-03-26 Cristian Valencia <chpayan@unicauca.edu.co>
+******************************************************************************
+*/
 
 #ifndef LCD_H
-#define LCD_H
+#define	LCD_H
 
-#ifdef __cplusplus
+#ifdef	__cplusplus
 extern "C" {
 #endif
 
-#include <xc.h>
-#include "Config.h"
-#include <pic18f4550.h>
-#include <stdint.h>
+#include <xc.h> // include processor files - each processor file is guarded.  
 
-/*********************Definition of Ports********************************/
-#define RS LATD0
-#define EN LATD1
-#define ldata LATD
+#include <pic18f4550.h>
+#include "Config.h"
+    
+    /*********************Definition of Ports********************************/
+
+#define RS LATD0                    /*PIN 0 of PORTB is assigned for register select Pin of LCD*/
+#define EN LATD2                  /*PIN 1 of PORTB is assigned for enable Pin of LCD */
+#define ldata LATD                  /*PORTB(PB4-PB7) is assigned for LCD Data Output*/ 
 #define LCD_Port TRISD
    
 #define CMD_CLEAR_LCD 0x01
+    
+void LCD_Init();                    /*Initialize LCD*/
+void LCD_Command(unsigned char );   /*Send command to LCD*/
+void LCD_Char(unsigned char x);     /*Send data to LCD*/
+void LCD_String(const char *);      /*Display data string on LCD*/
+void LCD_String_xy(char, char , const char *);
+void LCD_Clear();                   /*Clear LCD Screen*/
 
-void LCD_Init(void);
-void LCD_Command(uint8_t cmd);
-void LCD_Char(uint8_t dat);
-void LCD_String(const char *msg);
-void LCD_String_xy(uint8_t row, uint8_t pos, const char *msg);
-void LCD_Clear(void);
 
-#ifdef __cplusplus
+#ifdef	__cplusplus
 }
 #endif
 
-#endif
+#endif	/* LCD_H */
+
+
