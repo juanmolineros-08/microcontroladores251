a3b8f5389c595b85b8e0fcef065b5e0a1a1fc4f0
Miguel Tapia <143210047+Miguel072@users.noreply.github.com>
2025-05-09 18:13:17 -0500

    ProyectoSegundoCorte
    incluye sensores de temperatura y humedad, RTC, y el modulo bluetooth, funcionando juntos en un archivo llamada conjunto.c

---
+++ estacion.X/DTH11.h
@@ -0,0 +1,15 @@
+
+#ifndef DHT11_H
+#define DHT11_H
+
+#include <xc.h>
+#include <stdint.h>
+#include "Config.h"
+
+#define DHT11_PIN  PORTAbits.RA0    // Pin DATA en RA0
+#define DHT11_TRIS TRISAbits.TRISA0 
+
+uint8_t DHT11_Read();
+uint8_t DHT11_GetTemperature();
+
+#endif
